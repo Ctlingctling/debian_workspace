6b25e4bb061d53b142936142aa91bbfaf338891c
ZeroVect0r <fixtower@163.com>
2025-12-20 11:55:05 +0800

    how annoying.

--- program_dec20/list_oprt.c
+++ program_dec20/list_oprt.c
@@ -1,56 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct node {
-        char ele;
+        int  id;
+        char name[32];
+        int  age;
+        int  height;
         struct node * next;
 };
 
 int main()
 {
-        printf("debug.");
+        int n;
+        int m;
+
+        struct node *head = crt_list(n);
+        head = command(m);
+
+        for (; head->next != NULL; head = head->next) {
+                printf("%d %s %d %d", head->id, head->name, head->age, head->height);
+        }
 }
 
-struct node* crt_list (int len) 
+struct node *crt_list(int len)
 {
-        /**
-         * --Nodes--
-         *  We only need three nodes to creat a list.
-         */
-        
-        struct node* head = NULL;
-        struct node* new_node  = NULL;
-        struct node* tail = NULL;
+	struct node *head = NULL;
+	struct node *tail = NULL;
+	struct node *new_node = NULL;
+	char line_buf[256];
+	int i;
 
-        /**
-         * --A for loop--
-         * alloc space for each node,notice that the space isnt continuous.
-         * Since we malloc for a new node every time.
-         */
+	for (i = 0; i < len; i++) {
+		/* Read line first ensures we don't malloc if input is empty */
+		if (fgets(line_buf, sizeof(line_buf), stdin) == NULL)
+			break;
 
-        for (int i = 0; i < len; i++) {
-                /* now malloc a space, size of the node.*/
-                new_node = (struct node*)malloc(sizeof(struct node));
-                /* if theres no input,ignore it*/
-                if (scanf("%c", &(*new_node).ele) != 1)
-                        exit(0);
+		new_node = malloc(sizeof(*new_node));
+		if (!new_node)
+			exit(1);
 
-                /* the new node have to be the last one,pointing nothing.*/
-                new_node -> next = NULL;
+		/* Parsing: %31s ensures we don't overflow name[32] */
+		if (sscanf(line_buf, "%d %31s %d %d", 
+			   &new_node->id, new_node->name, 
+			   &new_node->age, &new_node->height) != 4) {
+			free(new_node);
+			continue;
+		}
 
-                /**
-                 * --the core part of the program.--
-                 */
+		new_node->next = NULL;
 
-                if (head == NULL) {
-                        head = new_node;
-                        tail = new_node;
-                } else {
-                        tail -> next = new_node;
-                        tail = new_node;
-                }
-        }
-        return head;
+		if (head == NULL) {
+			head = new_node;
+			tail = new_node;
+		} else {
+			tail->next = new_node;
+			tail = new_node;
+		}
+	}
+	return head;
 }
 
 struct node* del_list_head(struct node* head)
@@ -62,89 +70,93 @@ struct node* del_list_head(struct node* head)
         return new_head;
 }
 
-/**
- * struct node* del_list_tail(struct node* head, int len)
- * {
- *        if (head == NULL)
- *                return NULL;
- *        int count = 0;
- *        struct node* pre_node = head;
- *        while (count != len - 1) {
- *                pre_node = pre_node->next;
- *                count++;
- *        }
- *        free(pre_node->next);
- *        pre_node->next = NULL;
- *        return pre_node;
- * }
- */
-
-struct node* del_list_tail(struct node* head)
+struct node *insert(struct node *head, int x, int y)
 {
-        if (head == NULL) return NULL;
-        if (head->next == NULL) {
-                free(head);
-                return NULL;
-        }
-        struct node* temp = head;
-        while (temp->next->next != NULL) {
-                temp = temp->next;
-        }
-        free(temp->next);
-        temp->next = NULL;
-        return head;
+	struct node *curr = head;
+	struct node *node_x = NULL;
+	struct node *prev_x = NULL;
+	struct node *node_y = NULL;
+
+	/* Search phase */
+	while (curr) {
+		if (curr->id == y)
+			node_y = curr;
+		
+		if (curr->id == x)
+			node_x = curr;
+		else if (curr->next && curr->next->id == x)
+			prev_x = curr; /* prev_x found */
+
+		curr = curr->next;
+	}
+
+	/* Validation phase */
+	if (!node_x || !node_y || node_x == node_y)
+		return head;
+	
+	/* Detach phase */
+	if (node_x == head)
+		head = node_x->next;
+	else if (prev_x)
+		prev_x->next = node_x->next;
+	else
+		return head; /* Error: x found but cannot detach */
+
+	/* Insert phase */
+	node_x->next = node_y->next;
+	node_y->next = node_x;
+
+	return head;
 }
 
-struct node* reverse_list(struct node *head)
+struct node *delete(struct node *head, int num)
 {
-        if (head       == NULL) return NULL;
-        if (head->next == NULL) return head;
-        
-        struct node *pre  = NULL;
+        struct node *prev = NULL;
         struct node *curr = head;
-        struct node *temp;
-        while (curr != NULL) {
-                temp = curr->next;
-                curr->next = pre;
-                pre  = curr;
-                curr = temp;
-        }
-        return pre;
-}
 
-struct node* del_from_tail(struct node* head, int n)
-{
         if (!head)
                 return NULL;
-        if (!head->next) {
-                free(head);
-                return NULL;
+
+        if (curr->id == num) {
+                struct node *temp = curr->next;
+                free(curr);
+                return temp;
         }
 
-        struct node *curr = head;
-        int count  = 0;
-        int number = 0;
-        while (curr != NULL) {
-                count ++;
+        while (curr) {
+                if (curr-> id == num)
+                        break;
+                prev = curr;
                 curr = curr->next;
         }
 
-        number = count - n + 1;
-        count  = 1;
-        curr = head;
-        struct node *pre = NULL;
-
-        while (count < number) {
-                pre = curr;
-                curr = curr->next;
-                count ++;
-        }
-
-        struct node *tmp = curr->next;
+        prev->next = curr->next;
         free(curr);
-        pre->next = tmp;
-        free(tmp);
 
         return head;
+}
 
+struct node *command(struct node *head, int m)
+{
+        char buffer[256];
+        int type;
+        int numx;
+        int numy;
+
+        for (int i = 0; i < m; i++) {
+                if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+                        break;
+                }
+
+                if (sscanf(buffer, "%d %d %d", &type, &numx, &numy) != 3) {
+                        if (sscanf(buffer, "%d %d", &type, &numx) != 2) {
+                                continue;
+                        }
+                }
+
+                if (type == 1) head = insert (head, numx, numy);
+                if (type == 2) head = insert (head, numx, numy - 1);
+                if (type == 3) head = delete (head, numx);
+        }
+        return head;
 }

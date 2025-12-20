72e7b7e04381425d96fea0b8ffeb19377748608c
ZeroVect0r <fixtower@163.com>
2025-12-20 09:22:42 +0800

    completed a simple one

--- program_dec20/str_sort.c
+++ program_dec20/str_sort.c
@@ -3,26 +3,40 @@
 int main()
 {
         int n;
-        scanf("%d",&n);
+        scanf("%d", &n);
+        getchar();
 
         char str[n][101];
         for (int i = 0; i < n; i++) {
                 fgets(str[i], 101, stdin);
+                int len = strlen(str[i]);
+                if (len > 0 && str[i][len - 1] == '\n') {
+                        str[i][len - 1] = '\0';
+                }
         }
 
-        char tmp[101];
-        for (int i = 0; i < n; i++) {
-                for (int j = i; j < n; j++) {
+        char tmp [101];
+        
+        for (int i = 0; i < n - 1; i++) {
+                for (int j = i + 1; j < n; j++) {
                         if (strlen(str[i]) > strlen(str[j])) {
                                 strcpy(tmp, str[i]);
                                 strcpy(str[i], str[j]);
                                 strcpy(str[j], tmp);
+                        } else if (strlen(str[i]) == strlen(str[j])) {
+                                if (strcmp(str[i], str[j]) > 0) {
+                                        strcpy(tmp, str[i]);
+                                        strcpy(str[i], str[j]);
+                                        strcpy(str[j], tmp);
+                                }
                         }
                 }
         }
 
         for (int k = 0; k < n; k++) {
-                printf("%s", str[k]);
+                if (k == n) printf("%s", str[k]);
+                printf("%s\n", str[k]);
+                
         }
         return 0;
 }

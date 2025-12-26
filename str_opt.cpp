2db72bf3417a7c52b5a2d9999b54338514c6aef7
ZeroVect0r <fixtower@163.com>
2025-12-26 16:48:22 +0800

    nothing commited.

--- str_opt.cpp
+++ str_opt.cpp
@@ -1,21 +1,33 @@
 #include <bits/stdc++.h>
+
 using namespace std;
+
+void word_reverse();
+
 int main() {
         word_reverse();
+        return 0;
 }
 
 void word_reverse() {
         string line = "I am learning how to use cpp 12345";
         stringstream ss(line);
 
-        vector<string> word;
+        vector<string> words;
         string token;
 
         while (ss >> token) {
-                word.push_back(token);
+                words.push_back(token);
         }
-        for (string& w : word) {
+        for (string& w : words) {
+                cout << w << ' ';
+        }
+
+        cout << endl;
+
+        for (string& w : words) {
                 reverse(w.begin(), w.end());
                 cout << w << ' ';
         }
+        cout << endl;
 }
